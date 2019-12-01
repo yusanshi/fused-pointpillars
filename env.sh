b8043c426b8efffc6964b56a29877c26d85e36fc
yusanshi <yulei2018@gmail.com>
2019-12-02 01:23:34 +0800

    Fix warnging for numba

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-export NUMBAPRO_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
-export NUMBAPRO_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
-export NUMBAPRO_LIBDEVICE=/usr/local/cuda/nvvm/libdevice
+export NUMBA_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
+export NUMBA_NVVM=/usr/local/cuda/nvvm/lib64/libnvvm.so
+export NUMBA_LIBDEVICE=/usr/local/cuda/nvvm/libdevice
 export PYTHONPATH=/code/fused_pointpillars
 export KITTI_DATASET_ROOT=/kitti_dataset
